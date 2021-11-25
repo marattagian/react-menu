f90b457e66883e3b69e0b4558f06350008082463
Gianni Maratta <marattagian15@gmail.com>
2021-11-25 20:06:01 -0300

    adding the mock data in data.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Categories from "./components/categories"
+import Menu from "./components/menu"
 
 function App() {
 
   return (
     <div className="App">
+      <Menu />
+      <Categories />
     </div>
   )
 }
